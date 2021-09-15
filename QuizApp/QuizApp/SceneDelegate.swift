cfac1e310ddc27eefe5c46e82de5a72a5e25de53
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-15 15:49:42 +0400

    Add `QuizApp` project for UI.

---
+++ QuizApp/QuizApp/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  QuizApp
+//
+//  Created by Saba Khutsishvili on 15.09.21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
