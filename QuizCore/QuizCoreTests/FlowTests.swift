6e95468f8d7128e120c32004d73b4defdb48bae0
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-15 12:07:26 +0400

    Add  framework to cover core logic of app

---
+++ QuizCore/QuizCoreTests/FlowTests.swift
@@ -0,0 +1,15 @@
+//
+//  FlowTests.swift
+//  QuizCoreTests
+//
+//  Created by Saba Khutsishvili on 15.09.21.
+//
+
+import Foundation
+import XCTest
+
+class FlowTests: XCTestCase {
+    
+    
+    
+}
