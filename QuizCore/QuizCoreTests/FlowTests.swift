9043012fa7226e2bf6f04139d143d9883f082e9d
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-15 14:29:45 +0400

    Remove redundant test.

--- QuizCore/QuizCoreTests/FlowTests.swift
+++ QuizCore/QuizCoreTests/FlowTests.swift
@@ -47,15 +47,6 @@ class FlowTests: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q1"])
     }
     
-    func test_startAndAnswerFirstQuestion_withTwoQuestions_routesToSecondQuestion() {
-        let sut = makeSUT(questions: ["Q1", "Q2"])
-        
-        sut.start()
-        router.answerCallback("A1")
-        
-        XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
-    }
-    
     func test_startAndAnswerFirstTwoQuestions_withThreeQuestions_routesToSecondAndThirdQuestion() {
         let sut = makeSUT(questions: ["Q1", "Q2", "Q3"])
         
