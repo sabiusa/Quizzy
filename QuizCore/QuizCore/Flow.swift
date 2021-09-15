02dcf53078df9f10a06e18bf80ea54b0c19a45cf
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-15 12:20:53 +0400

    Flow with no questions does not route to any questions.

---
+++ QuizCore/QuizCore/Flow.swift
@@ -0,0 +1,27 @@
+//
+//  Flow.swift
+//  QuizCore
+//
+//  Created by Saba Khutsishvili on 15.09.21.
+//
+
+import Foundation
+
+protocol Router {
+    
+}
+
+class Flow {
+    
+    let router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func start() {
+        
+    }
+    
+}
+
--- QuizCore/QuizCoreTests/FlowTests.swift
+++ QuizCore/QuizCoreTests/FlowTests.swift
@@ -8,8 +8,21 @@
 import Foundation
 import XCTest
 
+@testable import QuizCore
+
 class FlowTests: XCTestCase {
     
+    func test_start_withNoQuestions_doesNoRouteToQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(router: router)
+        
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestionCount, 0)
+    }
     
+    private class RouterSpy: Router {
+        var routedQuestionCount = 0
+    }
     
 }
