7a8a1bf14ee971d41dc2fb4af2835188166f2681
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-15 16:42:51 +0400

    Refactor out SUT creation into a helper factory method.

--- QuizApp/QuizAppTests/QuestionViewControllerTests.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTests.swift
@@ -13,31 +13,25 @@ import XCTest
 class QuestionViewControllerTests: XCTestCase {
     
     func test_viewDidLoad_rendersQuestionHeaderText() {
-        let sut = QuestionViewController(question: "Q1", options: [])
+        let sut = makeSUT(question: "Q1")
         
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(sut.headerLabel.text, "Q1")
     }
     
-    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
-        let sut = QuestionViewController(question: "Q1", options: [])
+    func test_viewDidLoad_rendersCorrectNumberOfOptions() {
+        let sut1 = makeSUT(question: "Q1", options: [])
+        sut1.loadViewIfNeeded()
+        XCTAssertEqual(sut1.tableView.numberOfRows(inSection: 0), 0)
         
-        sut.loadViewIfNeeded()
-        
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
-    }
-    
-    func test_viewDidLoad_withOneOption_rendersOneOption() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        
-        sut.loadViewIfNeeded()
-        
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+        let sut2 = makeSUT(question: "Q1", options: ["A1", "A2"])
+        sut2.loadViewIfNeeded()
+        XCTAssertEqual(sut2.tableView.numberOfRows(inSection: 0), 2)
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOptionText() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        let sut = makeSUT(question: "Q1", options: ["A1"])
         
         sut.loadViewIfNeeded()
         
@@ -45,6 +39,16 @@ class QuestionViewControllerTests: XCTestCase {
         let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
         
         XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+    
+    // MARK:- Helpers
+    
+    func makeSUT(
+        question: String = "",
+        options: [String] = []
+    ) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options)
+        return sut
     }
     
 }
