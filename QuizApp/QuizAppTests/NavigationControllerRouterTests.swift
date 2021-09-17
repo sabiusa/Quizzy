72b39661289fcde8baf5b7c8e37915c5f4ddbb1e
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-17 09:10:21 +0400

    Routing to further questions, presents correct view controllers.

--- QuizApp/QuizAppTests/NavigationControllerRouterTests.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTests.swift
@@ -25,7 +25,29 @@ class NavigationControllerRouterTests: XCTestCase {
         
         sut.route(to: "Q1", answerCallback: { _ in })
         
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
         XCTAssertEqual(navigationController.viewControllers.first, viewController)
+    }
+    
+    func test_routeToSecondQuestion_presentsQuestionController() {
+        let navigationController = UINavigationController()
+        let factory = ViewControllerFactoryStub()
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondViewController)
+        
+        let sut = NavigationControllerRouter(
+            navigationController,
+            factory: factory
+        )
+        
+        sut.route(to: "Q1", answerCallback: { _ in })
+        sut.route(to: "Q2", answerCallback: { _ in })
+        
+        XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
     }
     
     // MARK:- Helpers
