8c59c90736849fff54c71ad22fa0bdaab44c7ce6
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-20 15:32:02 +0400

    Introduce new `QuizDelegate` protocol to replace `Router`.

--- QuizCore/QuizCore/Router.swift
+++ QuizCore/QuizCore/Router.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+public protocol QuizDelegate {
+    
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func handle(result: QuizResult<Question, Answer>)
+    
+}
+
 @available(*, deprecated)
 public protocol Router {
     
