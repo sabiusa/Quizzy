ac7d36ca5c6e578a89f867ee5dcd55ec24fd404b
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-20 17:40:13 +0400

    Renamed tests to clarify new intent.

--- QuizCore/QuizCoreTests/QuizTests.swift
+++ QuizCore/QuizCoreTests/QuizTests.swift
@@ -10,48 +10,48 @@ import QuizCore
 
 class QuizTests: XCTestCase {
     
-    private let router = RouterSpy()
-    private var game: Game<String, String, RouterSpy>!
+    private let delegate = DelegateSpy()
+    private var quiz: Game<String, String, DelegateSpy>!
     
     override func setUp() {
         super.setUp()
         
-        game = startGame(
+        quiz = startGame(
             questions: ["Q1", "Q2"],
-            router: router,
+            router: delegate,
             correctAnswers: ["Q1": "A1", "Q2": "A2"]
         )
     }
     
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
         
-        router.answerCallback("wrong")
-        router.answerCallback("wrong")
+        delegate.answerCallback("wrong")
+        delegate.answerCallback("wrong")
         
-        XCTAssertEqual(router.routedResult!.score, 0)
+        XCTAssertEqual(delegate.handledResult!.score, 0)
     }
     
-    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
+    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
         
-        router.answerCallback("A1")
-        router.answerCallback("wrong")
+        delegate.answerCallback("A1")
+        delegate.answerCallback("wrong")
         
-        XCTAssertEqual(router.routedResult!.score, 1)
+        XCTAssertEqual(delegate.handledResult!.score, 1)
     }
     
-    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
+    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
         
-        router.answerCallback("A1")
-        router.answerCallback("A2")
+        delegate.answerCallback("A1")
+        delegate.answerCallback("A2")
         
-        XCTAssertEqual(router.routedResult!.score, 2)
+        XCTAssertEqual(delegate.handledResult!.score, 2)
     }
     
     // MARK:- Helpers
     
-    private class RouterSpy: Router {
+    private class DelegateSpy: Router {
         
-        var routedResult: QuizResult<String, String>? = nil
+        var handledResult: QuizResult<String, String>? = nil
         
         var answerCallback: ((String) -> Void) = { _ in }
         
@@ -63,7 +63,7 @@ class QuizTests: XCTestCase {
         }
         
         func route(to result: QuizResult<String, String>) {
-            routedResult = result
+            handledResult = result
         }
         
     }
