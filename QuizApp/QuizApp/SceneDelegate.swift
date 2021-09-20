f85c6b34c25e57d20e11579082ae078e89e8c964
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-20 13:13:16 +0400

    Start game.

--- QuizApp/QuizApp/SceneDelegate.swift
+++ QuizApp/QuizApp/SceneDelegate.swift
@@ -6,49 +6,53 @@
 //
 
 import UIKit
+import QuizCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var game: Game<Question<String>, [String], NavigationControllerRouter>?
 
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        if let windowScene = scene as? UIWindowScene {
-            let newWindow = UIWindow(windowScene: windowScene)
-            newWindow.rootViewController = createResultsViewController()
-            
-            window = newWindow
-            window?.makeKeyAndVisible()
-        }
-    }
-    
-    private func createQuestionViewController() -> QuestionViewController {
-        return QuestionViewController(
-            question: "A question?",
-            options: ["Option 1", "Option 2", "Option 3"],
-            allowsMultipleSelection: false,
-            selection: { print($0) }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let question1 = Question.singleAnswer("What is 2x2?")
+        let question2 = Question.multipleAnswer("How to get 8")
+        let questions = [question1, question2]
+        
+        let option1 = "2", option2 = "4", option3 = "6"
+        let options1 = [option1, option2, option3]
+        
+        let option4 = "3 + 5", option5 = "2 + 7", option6 = "5 + 5", option7 = "0 + 8"
+        let options2 = [option4, option5, option6, option7]
+        let allOptions = [question1: options1, question2: options2]
+        
+        let correctAnswers = [
+            question1: [option2],
+            question2: [option4, option7]
+        ]
+        
+        let factory = iOSViewControllerFactory(
+            questions: questions,
+            options: allOptions,
+            correctAnswers: correctAnswers
         )
-    }
-    
-    private func createResultsViewController() -> ResultsViewController {
-        return ResultsViewController(
-            summary: "You got 1/2 correct",
-            answers: [
-                PresentableAnswer(
-                    question: "Question 1",
-                    correctAnswer: "Yes",
-                    wrongAnswer: nil
-                ),
-                PresentableAnswer(
-                    question: "Another Question",
-                    correctAnswer: "No",
-                    wrongAnswer: "Sad"
-                )
-            ]
+        let navigationController = UINavigationController()
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+        
+        let newWindow = UIWindow(windowScene: windowScene)
+        newWindow.rootViewController = navigationController
+        window = newWindow
+        window?.makeKeyAndVisible()
+        
+        game = startGame(
+            questions: questions,
+            router: router,
+            correctAnswers: correctAnswers
         )
     }
 
