c8585da67665e7a3cdad20c0cf4c0e958cc6a129
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-21 07:54:58 +0400

    Flow allows clients to answer same questions more than once.

--- QuizCore/QuizCoreTests/FlowTests.swift
+++ QuizCore/QuizCoreTests/FlowTests.swift
@@ -83,8 +83,8 @@ class FlowTests: XCTestCase {
     
     func test_startAndAnswerFirstQuestion_withTwoQuestions_doesNotCompleteQuiz() {
         let sut = makeSUT(questions: ["Q1", "Q2"])
-        
         sut.start()
+        
         delegate.answerCompletions[0]("A1")
         
         XCTAssertTrue(delegate.completedQuizzes.isEmpty)
@@ -92,13 +92,28 @@ class FlowTests: XCTestCase {
     
     func test_startAndAnswerFirstTwoQuestions_withTwoQuestions_completesQuiz() {
         let sut = makeSUT(questions: ["Q1", "Q2"])
-        
         sut.start()
+        
         delegate.answerCompletions[0]("A1")
         delegate.answerCompletions[1]("A2")
         
         XCTAssertEqual(delegate.completedQuizzes.count, 1)
         assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+    }
+    
+    func test_startAndAnswerFirstTwoQuestionsTwice_withTwoQuestions_completesQuizTwice() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
+        sut.start()
+        
+        delegate.answerCompletions[0]("A1")
+        delegate.answerCompletions[1]("A2")
+        
+        delegate.answerCompletions[0]("A1-1")
+        delegate.answerCompletions[1]("A2-2")
+        
+        XCTAssertEqual(delegate.completedQuizzes.count, 2)
+        assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+        assertEqual(delegate.completedQuizzes[1], [("Q1", "A1-1"), ("Q2", "A2-2")])
     }
     
     // MARK:- Helpers
