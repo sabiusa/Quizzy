6b1b9f2f3dc14005b0439fb3ce92491a1c719dac
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-21 08:20:49 +0400

    Scores zero for wrong one answer.

--- QuizCore/QuizCoreTests/ScoreTests.swift
+++ QuizCore/QuizCoreTests/ScoreTests.swift
@@ -10,14 +10,20 @@ import XCTest
 class ScoreTests: XCTestCase {
     
     func test_noAnswers_scoresZero() {
-        let score = BasicScore.score(for: [])
+        let score = BasicScore.score(for: [], comparingTo: [])
+        
+        XCTAssertEqual(score, 0)
+    }
+    
+    func test_oneWrongAnswer_scores_zero() {
+        let score = BasicScore.score(for: ["wrong"], comparingTo: ["right"])
         
         XCTAssertEqual(score, 0)
     }
     
     private class BasicScore {
         
-        static func score(for: [Any]) -> Int {
+        static func score(for: [Any], comparingTo: [Any]) -> Int {
             return 0
         }
         
