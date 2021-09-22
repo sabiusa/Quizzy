30d7b663374e0020817cc441438b4a2dc205bd51
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-22 12:09:29 +0400

    Remove redundant code.

--- QuizApp/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ QuizApp/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -17,10 +17,6 @@ struct MultipleSelectionStore {
         self.options = options.map(MultipleSelectionOption.init)
     }
     
-    func select(_ option: String) {
-        
-    }
-    
 }
 
 struct MultipleSelectionOption {
