8665e442b6465de45045615fbca411610f3fc1df
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-22 10:42:02 +0400

    Create `MultipleSelectionStoreTests` file.

---
+++ QuizApp/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  MultipleSelectionStoreTests.swift
+//  QuizAppTests
+//
+//  Created by Saba Khutsishvili on 22.09.21.
+//
+
+import XCTest
+
+class MultipleSelectionStoreTests: XCTestCase {
+    
+    
+    
+}
