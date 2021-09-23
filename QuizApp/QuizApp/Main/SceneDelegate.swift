1971119d85917b07ecabb71bd98bc3d6c236ab24
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-23 14:51:15 +0400

    Split UIKit and SwiftUI quiz composition.

--- QuizApp/QuizApp/Main/SceneDelegate.swift
+++ QuizApp/QuizApp/Main/SceneDelegate.swift
@@ -27,10 +27,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = newWindow
         window?.makeKeyAndVisible()
         
-        startNewQuiz()
+        startNewSwiftUIRouterQuiz()
     }
     
-    private func startNewQuiz() {
+    private func startNewUIKitQuiz() {
         let question1 = Question.singleAnswer("What is 2x2?")
         let question2 = Question.multipleAnswer("How to get 8")
         let questions = [question1, question2]
@@ -52,12 +52,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             correctAnswers: correctAnswers
         )
         
-//        let factory = iOSSwiftUIViewControllerFactory(
-//            navigation: navigationController,
-//            options: allOptions,
-//            correctAnswers: correctAnswers,
-//            playAgain: startNewQuiz
-//        )
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+        
+        quiz = Quiz.start(
+            questions: questions,
+            delegate: router
+        )
+    }
+    
+    private func startNewSwiftUIRouterQuiz() {
+        let question1 = Question.singleAnswer("What is 2x2?")
+        let question2 = Question.multipleAnswer("How to get 8")
+        let questions = [question1, question2]
+        
+        let option1 = "2", option2 = "4", option3 = "6"
+        let options1 = [option1, option2, option3]
+        
+        let option4 = "3 + 5", option5 = "2 + 7", option6 = "5 + 5", option7 = "0 + 8"
+        let options2 = [option4, option5, option6, option7]
+        let allOptions = [question1: options1, question2: options2]
+        
+        let correctAnswers = [
+            (question1, [option2]),
+            (question2, [option4, option7])
+        ]
+        
+        let factory = iOSSwiftUIViewControllerFactory(
+            options: allOptions,
+            correctAnswers: correctAnswers,
+            playAgain: startNewSwiftUIRouterQuiz
+        )
         
         let router = NavigationControllerRouter(navigationController, factory: factory)
         
