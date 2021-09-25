7edc785c4820662bb37c925a39a582dd5cbe9d8c
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-25 16:28:28 +0400

    create scene delegate programatically.

--- QuizApp/QuizApp/Main/AppDelegate.swift
+++ QuizApp/QuizApp/Main/AppDelegate.swift
@@ -24,7 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
     }
 
 }
