3de44fd9c5e9248fdf728d86f62462df396f6868
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-29 08:02:16 +0400

    Enforce every question to have at least one answer.

--- QuizApp/QuizAppTests/Scoring/TextualQuizBuilderTests.swift
+++ QuizApp/QuizAppTests/Scoring/TextualQuizBuilderTests.swift
@@ -11,19 +11,34 @@ import QuizCore
 struct TextualQuiz {
     
     let questions: [Question<String>]
+    let options: [Question<String>: [String]]
     
+}
+
+struct NonEmptyOptions {
+    let head: String
+    let tail: [String]
 }
 
 struct TextualQuizBuilder {
     
     private let questions: [Question<String>]
+    private let options: [Question<String>: [String]]
     
-    init(singleAnswerQuestion: String) {
-        questions = [.singleAnswer(singleAnswerQuestion)]
+    init(
+        singleAnswerQuestion: String,
+        options: NonEmptyOptions
+    ) {
+        let question = Question.singleAnswer(singleAnswerQuestion)
+        self.questions = [question]
+        self.options = [question: [options.head] + options.tail]
     }
     
     func build() -> TextualQuiz {
-        return TextualQuiz(questions: questions)
+        return TextualQuiz(
+            questions: questions,
+            options: options
+        )
     }
     
 }
@@ -31,11 +46,15 @@ struct TextualQuizBuilder {
 class TextualQuizBuilderTests: XCTestCase {
     
     func test_initWithSingleAnswerQuestion() {
-        let sut = TextualQuizBuilder(singleAnswerQuestion: "Q1")
+        let sut = TextualQuizBuilder(
+            singleAnswerQuestion: "Q1",
+            options: NonEmptyOptions(head: "O1", tail: ["O2", "O3"])
+        )
         
         let quiz = sut.build()
         
         XCTAssertEqual(quiz.questions, [.singleAnswer("Q1")])
+        XCTAssertEqual(quiz.options, [.singleAnswer("Q1"): ["O1", "O2", "O3"]])
     }
     
 }
